ceb7a66c57406d9d40de46ecc42c0b12f7849d20
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-07-11 12:41:01 +0200

    Logic 2.0 services + dto

---
+++ src/main/java/com/example/pdfsplitterbackend/dto/PDFSplitRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.pdfsplitterbackend.dto;
+
+import com.example.pdfsplitterbackend.enums.SplitMode;
+import lombok.Value;
+
+import java.util.List;
+
+@Value
+public class PDFSplitRequestDTO {
+    String pdfFileId;
+    SplitMode splitMode;
+    int pageNumberFrom;
+    int pageQuantityGroup;
+    int splitSizeKb;
+    List<Integer> splitPagesList;
+
+
+}
---
+++ src/main/java/com/example/pdfsplitterbackend/service/PDFSplitService.java
@@ -0,0 +1,5 @@
+package com.example.pdfsplitterbackend.service;
+
+public interface PDFSplitService {
+
+}
