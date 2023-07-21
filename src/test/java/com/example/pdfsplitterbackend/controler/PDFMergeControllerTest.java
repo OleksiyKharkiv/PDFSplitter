3c34414b2119a01bd5947354d86a31d4c83369ac
Oleksiy Khudoshyn <hudoshin7605@gmail.com>
2023-07-21 14:07:07 +0200

    Test 5.1

--- src/test/java/com/example/pdfsplitterbackend/controler/PDFMergeControllerTest.java
+++ src/test/java/com/example/pdfsplitterbackend/controler/PDFMergeControllerTest.java
@@ -1,14 +1,41 @@
 package com.example.pdfsplitterbackend.controler;
 
+import com.example.pdfsplitterbackend.controller.PDFMergeController;
+import com.example.pdfsplitterbackend.dto.PDFFileDTO;
+import com.example.pdfsplitterbackend.service.PDFFileService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(PDFMergeController.class)
-public class PDFMergeControllerTests {
+public class PDFMergeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private PDFFileService pdfFileService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void testMergePDFFiles() throws Exception {
@@ -19,9 +46,9 @@ public class PDFMergeControllerTests {
 
         mockMvc.perform(post("/api/pdf/merge")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(fileIds)))
+                        .content(objectMapper.writeValueAsString(fileIds)))
                 .andExpect(status().isOk())
-                .andExpect(content().string(mergedFileId));
+                .andExpect((ResultMatcher) content().string(mergedFileId));
     }
 
     @Test
@@ -36,6 +63,6 @@ public class PDFMergeControllerTests {
         mockMvc.perform(get("/api/pdf/merge/{id}", fileId))
                 .andExpect(status().isOk())
                 .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"merged.pdf\""))
-                .andExpect(content().bytes(fileContent));
+                .andExpect((ResultMatcher) content().bytes(fileContent));
     }
 }
